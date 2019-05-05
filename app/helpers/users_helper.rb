d2241b9e010b2f3a278b4227233df24c86d2afa7
igacio <ignacio.olea.c@gmail.com>
2019-05-05 16:40:10 -0400

    More styling, fixed update on user avatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,7 +3,7 @@ module UsersHelper
 			if user.avatar.attached?
 			 	image_tag(user.avatar, size:'100x100')
 			else
-				image_tag('https://www.shareicon.net/data/256x256/2015/10/02/649910_user_512x512.png', size:'100x100')
+				image_tag('https://www.shareicon.net/data/256x256/2015/10/02/649910_user_512x512.png',alt: '', size:'100x100')
 			end
 		end
 
