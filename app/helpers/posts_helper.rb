3609c538177b3403318ee6b025ba6114fc5c5a5e
igacio <ignacio.olea.c@gmail.com>
2019-05-06 01:29:01 -0400

    Style Post,User show and index method,fixed thumbnail size in helper

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,9 +1,9 @@
 module PostsHelper
-	def post_thumbnail(post)
+	def post_thumbnail(post,height,width)
 		if post.thumbnail.attached?
-			image_tag(post.thumbnail, style:'width:100%')
+			image_tag post.thumbnail.variant(resize: "#{height}x#{width}!")
 		else
-      image_tag('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTStpHWnWiVxtiAbTZMr81Ve5UBYdqn7lx8MvqJKzJpYBGrIrP', style:'width:100%')
+      image_tag('https://d9qzjwuieyamt.cloudfront.net/res/images/items/450default_sd.jpg?ih20190211')
 		end
 	end
 end
